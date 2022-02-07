2c350452e9362857f0ad753fefa3cbc19f3a2b70
yytugbaa <98499471+yytugbaa@users.noreply.github.com>
2022-02-07 15:30:12 +0300

    Create dog-years.js

---
+++ dog-years.js
@@ -0,0 +1,11 @@
+// the value of my name
+const myAge = 20;
+// the value of early years 
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2; 
+laterYears *= 4;
+console.log(earlyYears)
+console.log(laterYears)
+var myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears)
